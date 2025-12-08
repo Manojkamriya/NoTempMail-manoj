3dc417e406a1492357c961cad321463aa8cd98b9
Manojkamriya <kamriyamanoj45@gmail.com>
2025-12-08 23:49:21 +0530

    UI is cleaned

--- src/App.jsx
+++ src/App.jsx
@@ -15,22 +15,7 @@ function App() {
    {/* <TestImageChat/> */}
    <Versions/>
  {/* <Version2/> */}
- <div class="w-full h-170 bg-[linear-gradient(225deg,_#f8e9e8_0%,_white_40%,_#f8e9e8_100%)]">
-</div>
-<div className="relative rounded-2xl m-12">
-  {/* gradient border layer */}
-  <div className="absolute inset-0 rounded-2xl p-[4px] bg-[linear-gradient(90deg,_#75B7FF,_#D5A8FF,_#FF9BAD,_#FFD1A3)]"></div>
 
-  {/* blur/glow layer (softens the edges) */}
-  <div className="absolute inset-0 rounded-2xl pointer-events-none">
-    <div className="absolute inset-0 rounded-2xl blur-md opacity-30 bg-[linear-gradient(90deg,_#75B7FF,_#D5A8FF,_#FF9BAD,_#FFD1A3)]"></div>
-  </div>
-
-  {/* content */}
-  <div className="relative bg-white rounded-2xl p-6">
-    Your content here
-  </div>
-</div>
 
 
 
